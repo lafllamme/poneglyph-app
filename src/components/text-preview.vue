bc6d3c8e600052b7e88e68fd2d3631a6282599e9
Lafllamme <teke900@gmail.com>
2023-06-11 19:39:25 +0200

    update text preview component
    Improves the preview for the user's input.

--- src/components/text-preview.vue
+++ src/components/text-preview.vue
@@ -2,19 +2,25 @@
 import {storeToRefs} from "pinia";
 import {useFontStore} from "@/stores/useFont.store";
 
+
+
 const {fontText} = storeToRefs(useFontStore());
 
 </script>
 <template>
-  <div id="text-preview">
-    <div class="text-6xl text-gray-900 dark:text-white">{{ fontText }}</div>
-  </div>
+    <div class="text-preview py-2 px-5">
+      <textarea
+          v-model="fontText"
+          id="text-preview"
+          rows="12"
+          class="text-6xl text-center text-gray-900 dark:text-white block p-2.5 w-full bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
 </template>
 <style lang="scss">
 
-#text-preview {
+.text-preview {
   font-family: 'Poneglyph Regular', sans-serif;
   animation: fade-in 1.9s ease-in;
-  @apply text-center py-12 px-5;
 }
 </style>
