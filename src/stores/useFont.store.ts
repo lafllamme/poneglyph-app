6aaf5412b03d16666b09d17ab77a91226f18a445
Lafllamme <teke900@gmail.com>
2023-06-11 18:35:22 +0200

    add new helper to store
    This adds the needed logic to sanitize the input.

--- src/stores/useFont.store.ts
+++ src/stores/useFont.store.ts
@@ -1,14 +1,19 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import {computed, ref} from 'vue'
+import {defineStore} from 'pinia'
 
 export const useFontStore = defineStore('font', () => {
     const fontText = ref<string>('Hello World');
 
     const setFontText = (text: string) => {
-        fontText.value = text;
+        fontText.value = validateFontText(text);
     }
 
     const getFontText = computed(() => fontText.value);
 
-    return { fontText, setFontText, getFontText }
+    const validateFontText = ((text: string) => {
+        return text.replace(/[^a-zA-Z0-9\s]/g, '');
+    })
+
+
+    return {fontText, setFontText, getFontText, validateFontText}
 })
